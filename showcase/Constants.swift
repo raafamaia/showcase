86f4445aad725a08ecaa2b27c7e032f8b18193ed
Maia <raafamaia@gmail.com>
2016-10-02 21:10:33 -0300

    login interface

---
+++ showcase/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  showcase
+//
+//  Created by R. Maia on 02/10/16.
+//  Copyright © 2016 RM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
